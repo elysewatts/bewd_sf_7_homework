91c792b3c41cdd1c20c73ece3be78c4b7950727d
Andersen Yu <andersenyu0716@gmail.com>
2015-01-07 19:06:07 -0800

    adding more to views

--- andersen_yu/class_exercises/week 9/game_app/app/controllers/rock_paper_scissors_controller.rb
+++ andersen_yu/class_exercises/week 9/game_app/app/controllers/rock_paper_scissors_controller.rb
@@ -6,6 +6,8 @@ class RockPaperScissorsController < ApplicationController
   def show
   	@game_throw = get_throws[rand(3)]
   	@user_throw = get_throws[params[:id].to_i]
+    @user_image = get_image(@user_throw)
+    @comp_image = get_image(@game_throw)
     game_result(@user_throw, @game_throw)
   end
 
@@ -24,6 +26,16 @@ class RockPaperScissorsController < ApplicationController
       @results = "You've Lost!"
     end
       
+  end
+
+  def get_image(throw)
+    if throw == "Rock"
+      "http://www.clker.com/cliparts/p/G/a/s/t/g/rock-th.png"
+    elsif throw == "Paper"
+      "http://www.clker.com/cliparts/Y/t/o/V/q/F/paper-th.png"
+    else
+      "http://www.clker.com/cliparts/8/B/i/M/Y/Z/scissors-th.png"  
+    end
   end
 
 end
