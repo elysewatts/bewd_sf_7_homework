61be96654e60a8fe8e63587ded40e662dace07e1
atsoy04 <atsoy@zendesk.com>
2015-01-19 20:54:45 -0800

    adding blog and music app

---
+++ anna_tsoy/week_12_homework/music_app/app/controllers/songs_controller.rb
@@ -0,0 +1,38 @@
+class SongsController < ApplicationController
+	def index
+		@songs = Song.all
+	end
+
+	def new
+		@song = Song.new
+	end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @artist = Artist.find(params[:artist_id])
+    @song = @artist.songs.create(song_params)
+    redirect_to artist_path(@artist)
+  end
+
+  def edit
+  	@song = get_song
+  end
+
+  def update
+    @song = get_song
+    @new_song = @song.update(song_params)
+    redirect_to artist_song_path
+  end
+
+  private
+    def song_params
+      params.require(:song).permit(:name)
+    end
+
+    def get_song
+    	@song = Song.find(params[:id])
+    end
+end
